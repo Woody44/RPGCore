d45de9e541fb2655577b3e459ff1e2c74cfa8ee3
Woody44 <woodpecker_company@wp.pl>
2020-07-27 15:47:44 +0200

    2.0b2
    x kurwa D

--- src/com/rpg/core/events/Basics.java
+++ src/com/rpg/core/events/Basics.java
@@ -1,19 +1,36 @@
 package com.rpg.core.events;
 
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_16_R1.entity.CraftPlayer;
+import org.bukkit.entity.Cat;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import com.rpg.core.CoreConfig;
+import com.rpg.core.Main;
 import com.rpg.core.framework.StringManager;
+
+import net.minecraft.server.v1_16_R1.EntityCat;
+import net.minecraft.server.v1_16_R1.EntityLiving;
+import net.minecraft.server.v1_16_R1.PacketPlayOutEntityDestroy;
+
 import com.rpg.core.framework.PlayerManager;
 
 public class Basics implements Listener
 {
+	
+	public static ArrayList<Player> sneakedPlayers = new ArrayList<Player>();
 	@EventHandler
 	public void OnHunger(FoodLevelChangeEvent e)
 	{
@@ -47,5 +64,28 @@ public class Basics implements Listener
 	{
 		e.getDrops().clear();
 		e.setDroppedExp(0);
+	}
+	
+	@EventHandler
+	public void OnPlayerSneak(PlayerToggleSneakEvent e)
+	{
+		Player player = e.getPlayer();
+		if(!sneakedPlayers.contains(player)) {
+			PacketPlayOutEntityDestroy destroyPacket = new PacketPlayOutEntityDestroy(player.getEntityId());
+			World world = player.getWorld();
+			EntityLiving ent = null;
+			ent = new EntityCat(null, ((CraftPlayer) player).getHandle().world);
+			for(Player p : Bukkit.getOnlinePlayers())
+				p.hidePlayer(Main.GetMe(), player);
+			sneakedPlayers.add(player);
+			
+			Cat cat = (Cat)player.getWorld().spawnEntity(player.getLocation(), EntityType.CAT);
+		}
+		else
+		{
+			for(Player p : Bukkit.getOnlinePlayers())
+				p.showPlayer(Main.GetMe(), player);
+			sneakedPlayers.remove(player);
+		}
 	}
 }
